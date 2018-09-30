886a50894404fe0a9fe995cc970a7c97a9d178f0
Agnes Konradsdottir <agneskonradsdottir@gmail.com>
2018-09-30 20:44:06 +0200

    [WIP] Add hamburger menu

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,9 @@
 // This is where it all goes :)
+
+function openNav() {
+  document.getElementById("menu").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("menu").style.width = "0";
+}
