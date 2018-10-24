2654c30b2f723fad642b7b568f7d664b38f162f0
Agnes Konradsdottir <agneskonradsdottir@gmail.com>
2018-10-24 21:19:55 +0200

    Add toggleClass to sidebar

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,13 +1,20 @@
 // This is where it all goes :)
 
 document.addEventListener('DOMContentLoaded', () => {
-
   const nav__icon = document.querySelector(".nav");
   const ul = document.querySelector('.nav__items')
+
 
   nav__icon.addEventListener('click', event => {
     ul.classList.toggle('open');
     nav__icon.classList.toggle('open');
   })
 
+  
+
 })
+
+function toggleClass() {
+  document.querySelector(".nav").classList.toggle('open');
+  document.querySelector('.nav__items').classList.toggle('open');
+  }
